11465ead50f65c08d10384054ffc8a863c9762d9
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-03-13 11:33:31 +0100

    finish netflux polyfill

--- netflux/client.js
+++ netflux/client.js
@@ -1,36 +1,79 @@
 define([], function () {
     return {
         connect: function (webSocketURL) {
+          console.log('Signaling URL = ' + webSocketURL)
             var wc = netflux.create({
-              signaling: webSocketURL
+                signalingURL: webSocketURL
             });
-            return wc.open()
+            return wc.join('azerty')
                 .then(function () {
-                    return wc.join('azerty')
-                        .then(function () {
-                            return {
-                                webChannels: [],
-                                getLag: function getLag() {
-                                  return 50;
-                                },
-                                sendto: function sendto(peerId, content) {
-                                    wc.sendTo(peerId, content);
-                                    return Promise.resolve();
-                                },
-                                join: function join(chanId) { throw new Error("this is where we expect it to fail, not implemented yet.") },
-                                on: function (eventName, cb) {
-                                    switch (eventName) {
-                                        case 'message':
-                                            wc.onMessage = function (peerId, msg) {
-                                                cb(msg, peerId);
-                                            }
-                                            break;
-                                        case 'disconnect':
-                                            wc.onDisconnect = cb;
+                    console.log('Joining "azerty"')
+                    return {
+                        webChannels: [],
+                        getLag: function getLag() {
+                            return 50;
+                        },
+                        sendto: function sendto(peerId, content) {
+                            console.log('Sending |' + content + '| to ' + peerId)
+                            wc.sendTo(String(peerId), content);
+                            return Promise.resolve();
+                        },
+                        join: function join(chanId) {
+                            // this is where we expect it to fail, not implemented yet.
+                            return Promise.resolve({
+                              id: String(wc.myId),
+                              members: wc.members.map(function(peerId) {
+                                return String(peerId)
+                              }),
+                              bcast: function (content) {
+                                wc.send(content)
+                                return Promise.resolve()
+                              },
+                              leave: function (reason) {
+                                ws.leave()
+                              },
+
+                              on: function (eventName, cb) {
+                                  switch (eventName) {
+                                      case 'message':
+                                          wc.onMessage = function (peerId, msg, isBroadcast) {
+                                              if (isBroadcast) {
+                                                  console.log('Broadcast Message |' + msg + '| from ' + peerId)
+                                                  cb(msg, String(peerId));
+                                              }
+                                          }
+                                          break;
+                                      case 'join':
+                                          wc.onPeerJoin = function (peerId) {
+                                            console.log('Joined >' + peerId)
+                                            cb(String(peerId))
+                                          }
+                                          break;
+                                      case 'leave':
+                                          wc.onPeerLeave = function (peerId) {
+                                            console.log('Left >' + peerId)
+                                            cb(String(peerId), 'with no reason')
+                                          }
+                                  }
+                              }
+                            })
+                        },
+                        on: function (eventName, cb) {
+                            switch (eventName) {
+                                case 'message':
+                                    wc.onMessage = function (peerId, msg, isBroadcast) {
+                                        if (!isBroadcast) {
+                                            console.log('Personal message |' + msg + '| from ' + peerId)
+                                            cb(msg, String(peerId));
+                                        }
                                     }
-                                }
+                                    break;
+                                case 'disconnect':
+                                    // wc.onDisconnect = cb;
+                                    // TODO: implement this event
                             }
-                        })
+                        }
+                    }
                 })
         }
     };
